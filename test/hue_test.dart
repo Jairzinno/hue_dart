862b62e0b445bb67b48c844248ff94f4a69be0d6
Jairzinno Henriquez <jairzinnohenriquez@gmail.com>
2018-07-15 12:28:58 +0200

    renamed file

--- test/hue_test.dart
+++ test/hue_test.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart';
-import 'package:hue_dart/hue.dart';
+import 'package:hue_dart/hue_dart.dart';
 import 'package:hue_dart/src/core/bridge_client.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
