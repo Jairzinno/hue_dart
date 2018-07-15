ddfa22cd81ad25c66d2a035f999ec6625cf5af1d
Jairzinno Henriquez <jairzinnohenriquez@gmail.com>
2018-07-15 11:26:43 +0200

    export classes

--- lib/hue.dart
+++ lib/hue.dart
@@ -1,5 +1,16 @@
 /// Support for working with Philips Hue bridges
-///
+
 library hue_dart;
 
+export 'src/core/bridge.dart';
+export 'src/core/bridge_discovery.dart';
+export 'src/core/bridge_exception.dart';
+export 'src/configuration/configuration.dart';
+export 'src/group/group.dart';
+export 'src/light/light.dart';
+export 'src/resource_link/resource_link.dart';
+export 'src/rule/rule.dart';
+export 'src/scene/scene.dart';
+export 'src/schedule/schedule.dart';
+export 'src/sensor/sensor.dart';
 
